0055f75d7e883d8037d973f34a2866940089e77a
justmacn <172618812+justmacn@users.noreply.github.com>
2024-07-05 17:42:43 -0400

    added logic.js click event for color mode // JS

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,20 @@
-console.log('Hello world from logic');
+// declares global variables
+const modeSwitch = document.querySelector('#mode-switcher');
+const modeContainer = document.querySelector('.mode-container');
 
-//TODO on toggle, change class of div element to either dark or light
+// this variable represents our default color mode 
+let mode = 'light';
+
+// upon clicking, the event function will swap out the class of the page's color mode
+modeSwitch.addEventListener('click', function () {
+  // if the mode variable is already light it will reassign the value to dark
+  if (mode === 'light') {
+    mode = 'dark';
+    modeContainer.setAttribute('class', 'dark');
+  }
+  // if its dark it will switch to light
+  else {
+    mode = 'light';
+    modeContainer.setAttribute('class', 'light');
+  }
+});
